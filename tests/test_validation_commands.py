1e9796480fdc68303b261976f59348f87cceba96
bmallia <bmallia10@gmail.com>
2022-01-07 15:46:26 -0300

    Adicionando teste simulando primeiro comando diferente do PLACE

--- tests/test_validation_commands.py
+++ tests/test_validation_commands.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 from toyApp.processer import sender, CommandProcesser
-
+from toyApp.errors import FirstParameterError
 
 def test_parameter_list_command_example_a():
     """ Criação de teste de lista de comandos da referente ao arquivo B """
@@ -42,4 +42,14 @@ def test_parameter_list_command_across_board():
 
     sender(filename, v)    
     assert p.report() == "3,4,SOUTH"
+    
+def test_parameter_list_command_no_place():
+    """ Teste no caso de não ter o comando PLACE como o primeiro comando da lista de comandso """
+    with pytest.raises(FirstParameterError):
+        filename = os.path.dirname(__file__) + "/list_commands_e.txt"
+
+        p = CommandProcesser()
+        v = p.validate_commands()
+
+        sender(filename, v)    
     
