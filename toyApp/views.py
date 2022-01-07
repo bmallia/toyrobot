3b991ff62baf38b0964b333153be8c7d2dd68a56
bmallia <bmallia10@gmail.com>
2022-01-07 15:44:39 -0300

    Adicionando pagina html e controles para a movimentacao do robo no tabuleiro

--- toyApp/views.py
+++ toyApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+from toyApp.processer import CommandProcesser, sender_array
+from toyApp.errors import FirstParameterError
+
+def show_index(request):
+    """ Carrega a página do jogo """
+    return render(request, 'index.html')
+
+@csrf_exempt
+def process_commands(request):
+    """ processa os comandos da página web """
+    if request.method == 'POST':
+        try:
+            p = CommandProcesser()
+            v = p.validate_commands()
+            list_commands = request.POST.get('commands').split('\n')
+
+            sender_array(list_commands, v)
+            return HttpResponse(p.report())
+        except FirstParameterError as e:
+            return HttpResponse(json.dumps({"error": e.message }))
+    return HttpResponse(status=401)
--- toyrobo/urls.py
+++ toyrobo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from toyApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.show_index),
+    path('processcommands/', views.process_commands),
 ]
