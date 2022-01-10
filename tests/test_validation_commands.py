752acedd56b7c8246504d7fdde40a8626aeaefa8
bmallia <bmallia10@gmail.com>
2022-01-10 17:16:23 -0300

    Alterando teste

--- tests/test_validation_commands.py
+++ tests/test_validation_commands.py
@@ -3,6 +3,7 @@ import os
 from toyApp.processer import sender, CommandProcesser
 from toyApp.errors import FirstParameterError
 
+
 def test_parameter_list_command_example_a():
     """ Criação de teste de lista de comandos da referente ao arquivo B """
     filename = os.path.dirname(__file__) + "/list_commands_a.txt"
@@ -12,6 +13,7 @@ def test_parameter_list_command_example_a():
 
     sender(filename, v)
     assert p.report() == "0,1,SOUTH"
+
 
 def test_parameter_list_command_example_b():
     """ Criação de teste de lista de comandos da referente ao arquivo B """
@@ -23,6 +25,7 @@ def test_parameter_list_command_example_b():
     sender(filename, v)
     assert p.report() == "0,0,EAST"
 
+
 def test_parameter_list_command_example_c():
     """ Criação de teste de lista de comandos da referente ao arquivo C """
     filename = os.path.dirname(__file__) + "/list_commands_c.txt"
@@ -30,26 +33,19 @@ def test_parameter_list_command_example_c():
     p = CommandProcesser()
     v = p.validate_commands()
 
-    sender(filename, v)    
+    sender(filename, v)
     assert p.report() == "3,3,SOUTH"
 
-def test_parameter_list_command_across_board():
-    """ Criação de teste que simula a tentativa de ultrapassar o limite do tabuleiro """
-    filename = os.path.dirname(__file__) + "/list_commands_d.txt"
 
-    p = CommandProcesser()
-    v = p.validate_commands()
-
-    sender(filename, v)    
-    assert p.report() == "3,4,SOUTH"
-    
 def test_parameter_list_command_no_place():
-    """ Teste no caso de não ter o comando PLACE como o primeiro comando da lista de comandso """
+    """ Teste no caso de não ter o comando PLACE como o primeiro
+        comando da lista de comandso
+    """
     with pytest.raises(FirstParameterError):
         filename = os.path.dirname(__file__) + "/list_commands_e.txt"
 
         p = CommandProcesser()
         v = p.validate_commands()
 
-        sender(filename, v)    
+        sender(filename, v)
     
